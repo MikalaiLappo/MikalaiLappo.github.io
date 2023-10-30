0fef4fa189a925093155b7ec93fca447e3121a2c
Mikalai Lappo <mikalailappo@yandex.by>
2023-10-30 11:29:23 +0300

    feat: add Am component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,145 @@ module.exports = {
     './src/leaderboard/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      atten: '#383a58',
-      primary: '#E9A18D',
-      transparent: 'transparent',
-    },
-    backgroundColor: {
-      secondary: '#1d1b22',
-    },
-    textColor: {
-      white: '#fff',
-      secondary: '#b3afc0',
+    extend: {
+      colors: {
+        atten: '#383a58',
+        primary: '#E9A18D',
+        transparent: 'transparent',
+      },
+      backgroundColor: {
+        page: '#0e0d11',
+        secondary: '#1d1b22',
+      },
+      textColor: {
+        white: '#fff',
+        black: '#000',
+        secondary: '#b3afc0',
+        navlink: '#e4e2e9',
+      },
+      stroke: {
+        white: '#fff',
+        secondary: '#b3afc0',
+      },
+      transformOrigin: {
+        letter: '0 50%',
+      },
+      keyframes: {
+        am: {
+          '0%': {
+            opacity: '0',
+            content: "'Web Developer'",
+          },
+          '3%': {
+            opacity: '1',
+            content: "'Web Developer'",
+          },
+          '19%': {
+            opacity: '0',
+            content: "'Web Developer'",
+          },
+          '20%': {
+            opacity: '0',
+            content: "'TypeScript Wizard'",
+          },
+          '23%': {
+            opacity: '1',
+            content: "'TypeScript Wizard'",
+          },
+          '30%': {
+            opacity: '1',
+            content: "'TypeScript Wizard'",
+          },
+          '39%': {
+            opaicty: '0',
+            content: "'TypeScript Wizard'",
+          },
+          '40%': {
+            opacity: '0',
+            content: "'Hacker'",
+          },
+          '43%': {
+            opacity: '1',
+            content: "'Hacker'",
+          },
+          '50%': {
+            opacity: '1',
+            content: "'Hacker'",
+          },
+          '59%': {
+            opacity: '0',
+            content: "'Hacker'",
+          },
+          '60%': {
+            opacity: '0',
+            content: "'Linux Enthusiast'",
+          },
+          '63%': {
+            opacity: '1',
+            content: "'Linux Enthusiast'",
+          },
+          '70%': {
+            opacity: '1',
+            content: "'Linux Enthusiast'",
+          },
+          '79%': {
+            opacity: '0',
+            content: "'Linux Enthusiast'",
+          },
+          '80%': {
+            opacity: '0',
+            content: "'Perpetual Learner'",
+          },
+          '83%': {
+            opacity: '1',
+            content: "'Perpetual Learner'",
+          },
+          '90%': {
+            opacity: '1',
+            content: "'Perpetual Learner'",
+          },
+          '100%': {
+            opacity: '0',
+            content: "'Perpetual Learner'",
+          },
+        },
+        'am-bg': {
+          '0%': {
+            backgroundColor: '#bd93f9',
+          },
+          '19%': {
+            backgroundColor: '#bd93f9',
+          },
+          '20%': {
+            backgroundColor: '#67e1fb',
+          },
+          '39%': {
+            backgroundColor: '#67e1fb',
+          },
+          '40%': {
+            backgroundColor: '#ff5555',
+          },
+          '59%': {
+            backgroundColor: '#ff5555',
+          },
+          '60%': {
+            backgroundColor: '#f1fa8c',
+          },
+          '79%': {
+            backgroundColor: '#f1fa8c',
+          },
+          '80%': {
+            backgroundColor: '#50fa7b',
+          },
+          '100%': {
+            backgroundColor: '#50fa7b',
+          },
+        },
+      },
+      animation: {
+        am: 'am 20s ease-in infinite',
+        'am-bg': 'am-bg 20s ease-in infinite',
+      },
     },
   },
   plugins: [require('tailwind-dracula')('dracula', true)],
