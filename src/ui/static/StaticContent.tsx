d463afd8bbc3bddcd15a29fe60f882ef805520c6
Mikalai Lappo <mikalailappo@yandex.by>
2023-11-10 16:51:23 +0300

    fix: static content build-time typecheck

--- src/ui/static/StaticContent.tsx
+++ src/ui/static/StaticContent.tsx
@@ -1,15 +1,23 @@
 import { notFound } from 'next/navigation'
 import { Mdx } from './Mdx'
-import { allBlogPosts, allProjects } from 'contentlayer/generated'
+import {
+  BlogPost,
+  Project,
+  allBlogPosts,
+  allProjects,
+} from 'contentlayer/generated'
 
 type TContentDomain = 'blog' | 'projects'
 const getDocFromParams = async (domain: TContentDomain, slug: string) => {
   const content = domain === 'blog' ? allBlogPosts : allProjects
-  const doc = content.find((doc) => doc.link.endsWith(slug))
+  // Some kind of a bug? TS doesn't seem to spit any error/warning, yet Next.js build fails
+  // maybe something related to `Contentlayer` "linking" during bulid time
+  const doc = (content as any).find((doc: any) => doc.link.endsWith(slug))
 
   if (!doc) return notFound()
 
-  return doc
+  // manual `as` because of the bug above. still type-safe
+  return doc as Project | BlogPost
 }
 
 type IStaticContentProps = { params: any }
